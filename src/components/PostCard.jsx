266edcb6095529586429d89bb3d2ae730783dae4
hellentruong <hellentruong123@gmail.com>
2024-11-11 12:12:11 +0100

    flyttar koden lite i taget

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,22 @@
 import "../styles/stylePostCard.css";
 
-export default function PostCard() {
-  return <></>;
+export default function PostCard({ post, user }) {
+  return (
+    <>
+      <article>
+        <p>Användarnamn</p>
+        <h2>{post.title}</h2>
+        <p>{post.body.slice(0, 60)}...</p>
+
+        <ol>
+          {post.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ol>
+        {/* Likes måste vara vara dynamisk */}
+        <button>{post.reactions.likes}</button>
+        <button>Kommentar</button>
+      </article>
+    </>
+  );
 }
