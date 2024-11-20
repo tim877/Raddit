2c31aeb3ff2aa66f5495a473e97f87b7c84b8917
hellentruong <hellentruong123@gmail.com>
2024-11-20 09:31:21 +0100

    kommenterat ut testkod

--- src/atoms/users.jsx
+++ src/atoms/users.jsx
@@ -4,5 +4,25 @@ import { atom } from "recoil";
 export const usersState = atom({
     key: "users",
     default: []
-  });
+});
+
+// // initiala värdet är en tom array
+// const [users, setUsers] = useRecoilState(usersState);
+
+// // nya värdet är api datan som hämtas från DummyJSON
+// useEffect(() => {
+//   getAllUsers().then(setUsers);
+// }, [])
+
+
+// export const usernamesSelector = selector({
+//     key: "usernames",
+//     get: ({ get }) => {
+//       const usernames = get(users);
+//       // filtrera api arrayen
+//       return usernames.filter((username) => username.username);
+//     },
+// });
+
+
 
