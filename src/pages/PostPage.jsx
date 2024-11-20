80aa6aac5cecdedcfa0e77462967b3e4d96ed538
hellentruong <hellentruong123@gmail.com>
2024-11-20 11:59:22 +0100

    lagt till kommentar

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -7,7 +7,7 @@ import { getAllPosts } from "../api/posts.js";
 import Header from "../components/Header.jsx";
 import CommentList from "../components/CommentList.jsx";
 
-
+//optimera koden, finns nu dubletter av funktionerna handleUpVote och handleDownVote (i denna komponenten och i postCard-komponenten)
 export default function PostPage({pageData}) {
 
   const [posts, setPosts] = useRecoilState(postsState);
